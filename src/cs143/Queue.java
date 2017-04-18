baa61f6e81f84a91dac3dcf7db78fca18a602bc2
958016796 <958016796@LYN301-091.acs.edcc.ctc.edu>
2017-04-17 17:50:57 -0700

    Initializing the project 2 for java 143 class

---
+++ src/cs143/Queue.java
@@ -0,0 +1,55 @@
+package cs143;
+
+import java.util.LinkedList;
+
+/**
+ * This data-management class models a queue of generic objects by
+ * implementing the PureQueue interface.
+ *
+ * @param <E> the type of element to be saved in the queue
+ * 
+ * @author Phuc Hong Le 
+ * @author 
+ * @author 
+ * @author 
+ */
+public class Queue<E> implements PureQueue<E> {
+    
+    //fields
+    private final LinkedList<E> list;
+    
+    /**
+     * Constructor.
+     */
+    public Queue() {
+        
+    }
+    
+    // TODO implement all methods of the PureQueue interface //
+
+    @Override
+    public void enqueue(E e) {
+        
+    }
+
+    @Override
+    public E dequeue() {
+       
+    }
+
+    @Override
+    public boolean isEmpty() {
+ 
+    }
+
+    @Override
+    public E front() {
+ 
+    }
+
+    @Override
+    public int size() {
+  
+    }
+    
+}
